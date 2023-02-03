a3a121fc026563fdfb3e1062d4c731bb880c025a
alex_mountain <alexandermountain@gmail.com>
2023-02-03 15:38:02 -0800

    Reset to Fresh State for Project

---
+++ Assets/Scripts/AreaManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaManager : MonoBehaviour
+{
+    public static AreaManager Instance;
+
+    void Awake(){
+        Instance = this;
+    }
+
+    #TODO Reset Game Area 
+
+    
+}
---
+++ Assets/Scripts/ColonyAgent.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using Random = UnityEngine.Random;
+
+public class ColonyAgent : MonoBehaviour\+
+{
+
+    public override void Initialize()
+    {
+
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        var action = actionBuffers.DiscreteActions[0];
+        var targetPos = transform.position;
+
+        switch (action)
+        {
+            case k_NoAction:
+                break;
+            case k_Right:
+                targetPos = transform.position + new Vector3(1f, 0, 0);
+                break;
+            case k_Left:
+                targetPos = transform.position + new Vector3(-1f, 0, 0);
+                break;
+            case k_Up:
+                targetPos = transform.position + new Vector3(0, 0, 1f);
+                break;
+            case k_Down:
+                targetPos = transform.position + new Vector3(0, 0, -1f);
+                break;
+            default:
+                throw new ArgumentException("Invalid action value")
+        }
+
+        var hit = Physics.OverlapBox(targetPos, new Vector3(0.3f, 0.3f, 0.3f));
+        if (hit.Where(col => col.gameObject.CompareTag("wall")).ToArray().Length == 0)
+        {
+            transform.position = targetPos;
+
+            if (hit.Where(col => col.gameObject.CompareTag("food")).ToArray().Length == 1)
+            {
+                //TODO: Gain reward for collecting food
+            }
+            else if (hit.Where(col => col.gameObject.CompareTag("poison")).ToArray().Length == 1)
+            {   
+                //TODO: Gain negative reward for standing near or touching poison
+                //punishment should be proportional to distance
+            }
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        if (Input.GetKey(KeyCode.D))
+        {
+            continuousActionsOut[2] = 1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            continuousActionsOut[0] = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            continuousActionsOut[2] = -1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            continuousActionsOut[0] = -1;
+        }
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        discreteActionsOut[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        transform.position = new Vector3(Random.Range(-m_MyArea.range, m_MyArea.range),
+            2f, Random.Range(-m_MyArea.range, m_MyArea.range))
+            + area.transform.position;
+        transform.rotation = Quaternion.Euler(new Vector3(0f, Random.Range(0, 360)));
+    }
+}
