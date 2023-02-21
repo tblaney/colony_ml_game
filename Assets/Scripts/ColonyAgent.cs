123117619d5316db2e62fd2cb03b87cd3c2ee01b
Traven Blaney <travenblaney11@gmail.com>
2023-02-20 16:37:20 -0800

    heuristic working with correct step size

--- Assets/Scripts/ColonyAgent.cs
+++ Assets/Scripts/ColonyAgent.cs
@@ -15,42 +15,63 @@ public class ColonyAgent : Agent
     const int k_Left = 3;
     const int k_Right = 4;
 
+    LayerMask mask;
+
+    bool cooldown = false;
+    bool cooldown_2 = false;
+
     private EnvironmentParameters m_ResetParams;
 
     public override void Initialize()
     {
         m_ResetParams = Academy.Instance.EnvironmentParameters;
+
+        mask = LayerMask.GetMask("Obstacles");
     }
 
     //TODO: Make reward cumulative across all agents. (look up SharedReward() ML agents method)
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
+        //if (cooldown_2)
+        //    return;
+
         var action = actionBuffers.DiscreteActions[0];
         var targetPos = transform.position;
 
         AddReward(-0.005f);
+
+        bool moving = false;
 
         switch (action)
         {
             case k_NoAction:
                 break;
             case k_Right:
+                moving = true;
                 targetPos = transform.position + new Vector3(1f, 0, 0);
                 break;
             case k_Left:
+                moving = true;
                 targetPos = transform.position + new Vector3(-1f, 0, 0);
                 break;
             case k_Up:
+                moving = true;
                 targetPos = transform.position + new Vector3(0, 0, 1f);
                 break;
             case k_Down:
+                moving = true;
                 targetPos = transform.position + new Vector3(0, 0, -1f);
                 break;
             default:
                 throw new ArgumentException("Invalid action value");
         }
 
-        Collider[] hit = Physics.OverlapBox(targetPos, new Vector3(0.3f, 0.3f, 0.3f));
+        if (moving)
+        {
+            Debug.Log("Colony Agent Action Received: " + (targetPos - transform.position));
+        }
+
+        Collider[] hit = Physics.OverlapBox(targetPos, new Vector3(0.3f, 0.3f, 0.3f), Quaternion.identity, mask);
         if (hit.Where(col => col.gameObject.CompareTag("Wall")).ToArray().Length == 0)
         {
             transform.position = targetPos;
@@ -71,12 +92,29 @@ public class ColonyAgent : Agent
                 AddReward(-1f);
             }
         }
+
+        //if (cooldown && !cooldown_2)
+        //{
+        //    cooldown_2 = true;
+        //}//
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+        Debug.Log("Colony Agent Heuristic");
+
         var discreteActionsOut = actionsOut.DiscreteActions;
         discreteActionsOut[0] = k_NoAction;
+
+        if (cooldown)
+        {
+            //actionsOut.Clear();
+            return;
+        }
+        
+        cooldown = true;
+        Invoke("ResetCooldown", 0.1f);
+
         if (Input.GetKey(KeyCode.D))
         {
             discreteActionsOut[0] = k_Right;
@@ -93,5 +131,11 @@ public class ColonyAgent : Agent
         {
             discreteActionsOut[0] = k_Down;
         }
+    }
+
+    void ResetCooldown()
+    {
+        cooldown = false;
+        //cooldown_2 = false;
     }
 }
